9d24d1e5c90f9952e80829a679d962e4ec891f4e
Sofia Bargues <barguessofia@gmail.com>
2024-06-07 16:58:18 +0100

    esta ok

--- src/App.tsx
+++ src/App.tsx
@@ -9,37 +9,39 @@ function App() {
   return (
     <div className="flex flex-col border-red-500 border-2 max-w-[430px]  m-auto ">
       <img src={mobile}></img>
-      <div className="p-7 flex flex-col gap-7">
-        <h1 className="text-4xl font-bold">Stay updated!</h1>
-        <p>Join 60,000+ product managers receiving monthly updates on:</p>
-        <ul>
-          <div className="flex flex-row gap-3 items-center">
-            <img src={icon} className="h "></img>
+      <div className="p-7 flex flex-col gap-9">
+        <h1 className="text-5xl font-bold mt-3">Stay updated!</h1>
+        <p className="text-lg">
+          Join 60,000+ product managers receiving monthly updates on:
+        </p>
+        <ul className="text-lg flex-col flex gap-3">
+          <div className="flex flex-row gap-3 items-start">
+            <img src={icon} className=""></img>
             <li>Product discovery and building what matters</li>
           </div>
-          <div className="flex flex-row gap-3  items-center">
+          <div className="flex flex-row gap-3  items-start">
             <img src={icon} className=""></img>{" "}
             <li>Measuring to ensure updates are a success</li>
           </div>
-          <div className="flex flex-row gap-3  items-center">
+          <div className="flex flex-row gap-3  items-start">
             <img src={icon} className=""></img>
             <li>And much more!</li>
           </div>
         </ul>
-        <form className="flex flex-col gap- 3">
+        <form className="flex flex-col gap-3">
           <label className="text-[14px] font-semibold">Email address</label>
           <input
             placeholder="email@company.com"
-            className="W  border-2"
+            className="h-16 placeholder:pl-4 rounded-lg border-2"
             type="email"
             id="email"
             name="email"
             required
           ></input>
+          <button className="rounded-xl p-5 text-white bg-slate-600">
+            Subscribe to monthly newsletter
+          </button>
         </form>
-        <button className="rounded-xl p-4 text-white bg-slate-600">
-          Subscribe to monthly newsletter
-        </button>
       </div>
     </div>
   );
